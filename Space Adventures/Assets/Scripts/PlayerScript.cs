a02e84b3e06d2ffacc8fcd8c7f7162d83edd4950
PlatPlat48 <roberts.chance@gmail.com>
2017-02-20 22:03:33 -0700

    Player Gravity Test
    Added gravity to player, added adjustable gravity.

---
+++ Space Adventures/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	float dX, dY;
+	// Use this for initialization
+	void Start () {
+		dX = 0;
+		dY = 0;
+		Physics2D.gravity = new Vector2 (0, -gloVar.gravity);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void OnTriggerEnter(Collider2D other){
+		
+	}
+}
---
+++ Space Adventures/Assets/Scripts/gloVar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/*
+ * For global variables & functions
+ * I know it can be bad practice, but I feel like we may need some variables that can be modified 
+ * by one section and used in another section.
+ * So... yeah.
+ * */
+public class gloVar : MonoBehaviour {
+	public static float gravity = 10f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+		
+}
