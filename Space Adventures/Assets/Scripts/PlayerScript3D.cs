039d08d38f9b49c1c10caa133c0c02b8297d6134
Tamim Shaban <tamim.sha@gmail.com>
2017-02-27 19:32:12 -0700

    Procedural Level Test

--- Space Adventures/Assets/Scripts/PlayerScript3D.cs
+++ Space Adventures/Assets/Scripts/PlayerScript3D.cs
@@ -2,20 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour {
+public class PlayerScript3D : MonoBehaviour {
 	//float dX, dY;
 	bool onGround,leftWallCheck,rightWallCheck;
 	int spacePress;
 	float jumpForce, walkVelocity, runVelocity, maxVelocity, refinedJump;
-	Rigidbody rBody;
+	Rigidbody2D rBody;
 	// Use this for initialization
 	void Start () {
 		//dX = 0;
 		//dY = 0;
-		Physics.gravity = new Vector2 (0, -gloVar.gravity);
+		Physics2D.gravity = new Vector2 (0, -gloVar.gravity);
 		onGround = false;
 		spacePress = 0;
-		rBody = GetComponent<Rigidbody> ();
+		rBody = GetComponent<Rigidbody2D> ();
 		jumpForce = 250;
 		walkVelocity = 6;
 		runVelocity = walkVelocity * 1.5f;
@@ -32,7 +32,7 @@ public class PlayerScript : MonoBehaviour {
 		bool rightPress = Input.GetKey (KeyCode.RightArrow) || Input.GetKey (KeyCode.D);
 		bool upPress = Input.GetKey (KeyCode.UpArrow) || Input.GetKey (KeyCode.W);
 		bool downPress = Input.GetKey (KeyCode.DownArrow) || Input.GetKey (KeyCode.S);
-		Vector3 vel = rBody.velocity;
+		Vector2 vel = rBody.velocity;
 		//Determine if you're running first.
 		if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift)) {
 			maxVelocity = runVelocity;
@@ -43,68 +43,68 @@ public class PlayerScript : MonoBehaviour {
 		if (leftPress && !rightPress && !leftWallCheck) {
 			if (vel.x > -maxVelocity) {
 				if (onGround)
-					rBody.AddForce (new Vector3 (-10, 0));
+					rBody.AddForce (new Vector2 (-10, 0));
 				else
-					rBody.AddForce (new Vector3 (-4, 0));
+					rBody.AddForce (new Vector2 (-4, 0));
 			} else if (onGround) {
 				if (vel.x * 13 / 14 > -maxVelocity) {
-					rBody.velocity = new Vector3 (vel.x * 13 / 14, vel.y);
+					rBody.velocity = new Vector2 (vel.x * 13 / 14, vel.y);
 				} else {
-					rBody.velocity = new Vector3 (-maxVelocity, vel.y);
+					rBody.velocity = new Vector2 (-maxVelocity, vel.y);
 				}
 			}
 		} else if (rightPress && !leftPress && !rightWallCheck) {
 			if (vel.x < maxVelocity) {
 				if (onGround)
-					rBody.AddForce (new Vector3 (10, 0));
+					rBody.AddForce (new Vector2 (10, 0));
 				else
-					rBody.AddForce (new Vector3 (4, 0));
+					rBody.AddForce (new Vector2 (4, 0));
 			} else if (onGround) {
 				if (vel.x * 13 / 14 < maxVelocity) {
-					rBody.velocity = new Vector3 (vel.x * 13 / 14, vel.y);
+					rBody.velocity = new Vector2 (vel.x * 13 / 14, vel.y);
 				} else {
-					rBody.velocity = new Vector3 (maxVelocity, vel.y);
+					rBody.velocity = new Vector2 (maxVelocity, vel.y);
 				}
 			}
 		} else if (onGround){
-			rBody.velocity = new Vector3 (vel.x*13/14, vel.y);
+			rBody.velocity = new Vector2 (vel.x*13/14, vel.y);
 		}
 		//There's a possibility that the person may hit space when the player is close to the ground. Let's account for that.
 		if (Input.GetKey (KeyCode.Space)) {
 			spacePress += 1;
-        } else {
+		} else {
 			spacePress = 0;
 		}
 		//Wall slide
 		if (!onGround) {
 			if (leftWallCheck && leftPress) {
-				rBody.AddForce (new Vector3 (-1, -Physics2D.gravity.y * 1 / 3));
+				rBody.AddForce (new Vector2 (-1, -Physics2D.gravity.y * 1 / 3));
 				if (spacePress > 0 && spacePress <= gloVar.isPressed) {
 					spacePress = gloVar.isPressed + 1;
 					refinedJump = 0;
 					if (upPress)
-						rBody.AddForce (new Vector3 (jumpForce * 0.4f, jumpForce * 1.3f));
+						rBody.AddForce (new Vector2 (jumpForce * 0.4f, jumpForce * 1.3f));
 					else if (downPress)
-						rBody.AddForce (new Vector3 (jumpForce * 0.8f, -jumpForce * 0.5f));
+						rBody.AddForce (new Vector2 (jumpForce * 0.8f, -jumpForce * 0.5f));
 					else
-						rBody.AddForce (new Vector3 (jumpForce * 1.0f, jumpForce * 0.8f));
+						rBody.AddForce (new Vector2 (jumpForce * 1.0f, jumpForce * 0.8f));
 				}
 			} else if (rightWallCheck && rightPress) {
-				rBody.AddForce (new Vector3 (1, -Physics2D.gravity.y * 1 / 3));
+				rBody.AddForce (new Vector2 (1, -Physics2D.gravity.y * 1 / 3));
 				if (spacePress > 0 && spacePress <= gloVar.isPressed) {
 					spacePress = gloVar.isPressed + 1;
 					refinedJump = 0;
 					if (upPress)
-						rBody.AddForce (new Vector3 (-jumpForce * 0.4f, jumpForce * 1.3f));
+						rBody.AddForce (new Vector2 (-jumpForce * 0.4f, jumpForce * 1.3f));
 					else if (downPress)
-						rBody.AddForce (new Vector3 (-jumpForce * 0.8f, -jumpForce * 0.5f));
+						rBody.AddForce (new Vector2 (-jumpForce * 0.8f, -jumpForce * 0.5f));
 					else
-						rBody.AddForce (new Vector3 (-jumpForce * 1.0f, jumpForce * 0.8f));
+						rBody.AddForce (new Vector2 (-jumpForce * 1.0f, jumpForce * 0.8f));
 				}
 			}
 		}
 		if (onGround && spacePress > 0 && spacePress <= gloVar.isPressed) {
-			rBody.AddForce (new Vector3 (0, jumpForce));
+			rBody.AddForce (new Vector2 (0, jumpForce));
 			onGround = false;
 			spacePress = gloVar.isPressed + 1;
 			refinedJump = 0;
@@ -113,32 +113,32 @@ public class PlayerScript : MonoBehaviour {
 			if (refinedJump > 0.25)
 				refinedJump = -1;
 			if (spacePress > 0)
-				rBody.AddForce (new Vector3 (0, -Physics2D.gravity.y * 1.0f));
+				rBody.AddForce (new Vector2 (0, -Physics2D.gravity.y * 1.0f));
 			else
-				rBody.AddForce (new Vector3 (0, Physics2D.gravity.y * 0.4f));
+				rBody.AddForce (new Vector2 (0, Physics2D.gravity.y * 0.4f));
 		}
 	}
 
 	void OnCollisionStay(Collision collision){
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (0, 1)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (0, 1)) > Mathf.Sqrt(2)/2) {
 				onGround = true;
 			}
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (1, 0)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (1, 0)) > Mathf.Sqrt(2)/2) {
 				leftWallCheck = true;
 			}
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (-1, 0)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (-1, 0)) > Mathf.Sqrt(2)/2) {
 				rightWallCheck = true;
 			}
 	}
 
 	void OnCollisionExit(Collision collision){
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (0, 1)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (0, 1)) > Mathf.Sqrt(2)/2) {
 				onGround = false;
 			}
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (1, 0)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (1, 0)) > Mathf.Sqrt(2)/2) {
 				leftWallCheck = false;
 			}
-			if (Vector2.Dot (collision.contacts [0].normal, new Vector3 (-1, 0)) > Mathf.Sqrt(2)/2) {
+			if (Vector2.Dot (collision.contacts [0].normal, new Vector2 (-1, 0)) > Mathf.Sqrt(2)/2) {
 				rightWallCheck = false;
 			}
 	}
---
+++ Space Adventures/Assets/Scripts/lvlTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lvlTest : MonoBehaviour {
+
+    public Transform camTransform;
+    public float speed;
+
+    private Vector3 startPos;
+    public Transform spawnTransform;
+    public float platformGap;
+    public float[] platformGapRange;
+    public float[] spawnHeightRange;
+
+    public GameObject[] smallDebris;
+    private bool inTunnel = false;
+    public float tunnelLength;
+    public GameObject tunnel;
+
+	// Use this for initialization
+	void Start () {
+        startPos = spawnTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        camTransform.position += speed * Vector3.right;
+        float chanceTunnel = Random.Range(0f, 1f);
+        float gap = (inTunnel) ? platformGap + tunnelLength : platformGap;
+        float chance = (inTunnel) ? 1f : 0.9f;
+        if (spawnTransform.position.x - startPos.x > gap & chanceTunnel < chance) {
+            float height = Random.Range(spawnHeightRange[0], spawnHeightRange[1]);
+            float gapDelta = Random.Range(platformGapRange[0], platformGapRange[1]);
+            Instantiate(smallDebris[Random.Range(0, 3)], spawnTransform.position + height * Vector3.up
+            + gapDelta * Vector3.right, spawnTransform.rotation * Quaternion.AngleAxis(180, Vector3.right));
+            inTunnel = false;
+            startPos = spawnTransform.position;
+        }
+        else if(spawnTransform.position.x - startPos.x > gap & chanceTunnel >= 0.9f) {
+            inTunnel = true;
+            Instantiate(tunnel, spawnTransform.position, spawnTransform.rotation * Quaternion.AngleAxis(180, Vector3.right));
+            startPos = spawnTransform.position;
+        }
+        
+	}
+}
