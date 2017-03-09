cd912cdd3457399870e9ed960ce47d912ad28521
PlatPlat48 <roberts.chance@gmail.com>
2017-03-08 17:35:46 -0700

    Added a Ship
    Still need to add collision to the ship, though. Also need to have the
    warning signal follow the camera.

---
+++ Space Adventures/Assets/Scripts/Ship.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour {
+	private float maxSpeed;
+	private Rigidbody rBody;
+	// Use this for initialization
+	void Start () {
+		maxSpeed = 15.0f;
+		rBody = gameObject.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (rBody.velocity.x < -maxSpeed) {
+			rBody.velocity = new Vector3 (-maxSpeed, rBody.velocity.y);
+		} else {
+			rBody.AddForce (new Vector3 (-8, 0, 0));
+		}
+	}
+}
