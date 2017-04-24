cea2e914d52ab471dd724cad2cfa727c4d76bff0
kabo2956 <kabo2956@colorado.edu>
2017-04-23 19:11:32 -0600

    Relinked the menu to the game canvas with Resume and Exit buttons

---
+++ Space Adventures/Assets/Scripts/PauseGame.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseGame : MonoBehaviour {
+	public Transform canvas;
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			Pause ();
+		}
+	}
+	public void Pause(){
+
+		if (canvas.gameObject.activeInHierarchy == false) {
+			canvas.gameObject.SetActive (true);
+		} else {
+			canvas.gameObject.SetActive (false);
+		}
+
+	}
+}
