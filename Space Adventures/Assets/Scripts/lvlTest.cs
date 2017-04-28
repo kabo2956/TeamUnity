379d3b97870b1add0112800d02aae3e378878982
Tamim <tamim.sha@gmail.com>
2017-04-28 13:22:36 -0600

    Adding object pooling to lvl-test

--- Space Adventures/Assets/Scripts/lvlTest.cs
+++ Space Adventures/Assets/Scripts/lvlTest.cs
@@ -17,17 +17,42 @@ public class lvlTest : MonoBehaviour {
     public float[] spawnHeightRange;
 
     public GameObject[] smallDebris;
+    private GameObject[] smallDebrisSlice;
+    private GameObject[,] smallDebrisPool;
+    public int smallDebrisPoolSize = 10;
+    private int[] smallDebrisIndex;
+
 	public GameObject[] lasers;
-	public GameObject ship;
-	public GameObject asteroid;
-	public GameObject[] items;
+    private GameObject[,] lasersPool;
+    public int laserPoolSize = 10;
+    private int[] lasersPoolIndex;
+
+    public GameObject ship;
+    private GameObject[] shipPool;
+    private int shipPoolIndex = 0;
+    public int shipPoolSize = 5;
+
+    public GameObject asteroid;
+    private GameObject[] asteroidPool;
+    public int asteroidPoolSize = 5;
+    private int asteroidPoolIndex = 0;
+
+    public GameObject[] items;
+    private GameObject[,] itemsPool;
+    public int itemsPoolSize = 5;
+    private int[] itemPoolIndex;
+
     private bool inTunnel = false;
     public float tunnelLength;
 	public float shipSpawnMin, shipSpawnMax;
 	private float shipSpawn;
 	public float asteroidSpawnMin, asteroidSpawnMax;
 	private float asteroidSpawn;
+
     public GameObject tunnel;
+    private GameObject[] tunnelPool;
+    public int tunnelPoolSize = 3;
+    private int tunnelPoolIndex = 0;
 
 	/// <summary>
 	/// Use this for initialization
@@ -36,13 +61,82 @@ public class lvlTest : MonoBehaviour {
         startPos = spawnTransform.position;
 		shipSpawn = Random.Range (shipSpawnMin, shipSpawnMax);
 		asteroidSpawn = Random.Range (asteroidSpawnMin, asteroidSpawnMax);
-	}
+        Debug.Log("small debris");
+        smallDebrisPool = new GameObject[smallDebris.Length, smallDebrisPoolSize];
+        for(int i = 0; i < smallDebris.Length; i++)
+        {
+            for(int j = 0; j < smallDebrisPoolSize; j++)
+            {
+                smallDebrisPool[i, j] = Instantiate(smallDebris[i], new Vector3(0, 2000, 0), spawnTransform.rotation * Quaternion.AngleAxis(180, Vector3.right)) as GameObject;
+            }
+        }
+        smallDebrisSlice = new GameObject[smallDebrisPoolSize];
+        smallDebrisIndex = new int[smallDebris.Length];
+        for(int i = 0; i < smallDebris.Length; i++)
+        {
+            smallDebrisIndex[i] = 0;
+        }
+        Debug.Log("lasersPool");
+        lasersPool = new GameObject[lasers.Length, laserPoolSize];
+        for (int i = 0; i < lasers.Length; i++)
+        {
+            for (int j = 0; j < laserPoolSize; j++)
+            {
+                lasersPool[i, j] = Instantiate(lasers[i], new Vector3(0, 2000, 0), new Quaternion()) as GameObject;
+            }
+        }
+        lasersPoolIndex = new int[lasers.Length];
+        for (int i = 0; i < lasers.Length; i++)
+        {
+            lasersPoolIndex[i] = 0;
+        }
+        
+        Debug.Log("shipPool");
+        shipPool = new GameObject[shipPoolSize];
+        for (int i = 0; i < shipPoolSize; i++)
+        {
+            shipPool[i] = Instantiate(ship, new Vector3(0, 2000, 0), new Quaternion()) as GameObject;
+        }
+        /*
+        Debug.Log("asteroidPool");
+        asteroidPool = new GameObject[asteroidPoolSize];
+        for (int i = 0; i < asteroidPoolSize; i++)
+        {
+            asteroidPool[i] = Instantiate(asteroid, new Vector3(0, 2000, 0), new Quaternion()) as GameObject;
+        }
+        */
+        
+        Debug.Log("itemsPool");
+        itemsPool = new GameObject[items.Length, itemsPoolSize];
+        for (int i = 0; i < items.Length; i++)
+        {
+            for (int j = 0; j < itemsPoolSize; j++)
+            {
+                itemsPool[i, j] = Instantiate(items[i], new Vector3(0, 2000, 0), new Quaternion()) as GameObject;
+            }
+        }
+        itemPoolIndex = new int[items.Length];
+        for (int i = 0; i < items.Length; i++)
+        {
+            itemPoolIndex[i] = 0;
+        }
+        
+        Debug.Log("tunnelPool");
+        tunnelPool = new GameObject[tunnelPoolSize];
+        for (int i = 0; i < tunnelPoolSize; i++)
+        {
+            tunnelPool[i] = Instantiate(tunnel, new Vector3(0, 2000, 0), spawnTransform.rotation * Quaternion.AngleAxis(-90, Vector3.right)) as GameObject;
+        }
+        
+    }
 
-	/// <summary>
-	/// The main logic of the game is located here.
-	/// Controls what spawns when, and moves the screen and the wall.
-	/// Update is called once per frame
-	/// </summary>
+    /// <summary>
+    /// The main logic of the game is located here.
+    /// Controls what spawns when, and moves the screen and the wall.
+    /// Update is called once per frame
+    /// </summary>
+    /// 
+    
 	void Update () {
         camTransform.position += speed * Vector3.right;
         float chanceTunnel = Random.Range(0f, 1f);
@@ -51,17 +145,23 @@ public class lvlTest : MonoBehaviour {
         if (spawnTransform.position.x - startPos.x > gap & chanceTunnel < chance) {
             float height = Random.Range(spawnHeightRange[0], spawnHeightRange[1]);
             float gapDelta = Random.Range(platformGapRange[0], platformGapRange[1]);
-            Instantiate(smallDebris[Random.Range(0, 3)], spawnTransform.position + height * Vector3.up
-            + gapDelta * Vector3.right, spawnTransform.rotation * Quaternion.AngleAxis(180, Vector3.right));
+            //Instantiate(smallDebris[Random.Range(0, 3)], spawnTransform.position + height * Vector3.up
+            //+ gapDelta * Vector3.right, spawnTransform.rotation * Quaternion.AngleAxis(180, Vector3.right));
+            int rand = Random.Range(0, smallDebris.Length);
+            smallDebrisIndex[rand] = RepositionObject(GetGameObjectSlice(smallDebrisPool, smallDebrisSlice, rand, smallDebrisPoolSize), smallDebrisIndex[rand], smallDebrisPoolSize,
+                spawnTransform.position + height * Vector3.up + gapDelta * Vector3.right,
+                spawnTransform.rotation * (Quaternion.AngleAxis(180, Vector3.right)));
             inTunnel = false;
             startPos = spawnTransform.position;
         }
         else if(spawnTransform.position.x - startPos.x > gap & chanceTunnel >= 0.9f) {
             inTunnel = true;
-            GameObject g = Instantiate(tunnel, spawnTransform.position, spawnTransform.rotation * Quaternion.AngleAxis(-90, Vector3.right));
-			BoxCollider floor = g.GetComponentInChildren<BoxCollider> ();
-			Vector3 gPos = g.transform.position;
-			g.transform.position = new Vector3 (gPos.x+floor.size.y/2, gPos.y, gPos.z);
+            //GameObject g = Instantiate(tunnel, spawnTransform.position, spawnTransform.rotation * Quaternion.AngleAxis(-90, Vector3.right));
+            tunnelPoolIndex = RepositionObject(tunnelPool, tunnelPoolIndex, tunnelPoolSize, spawnTransform.position,
+                spawnTransform.rotation * Quaternion.AngleAxis(-90, Vector3.right));
+			BoxCollider floor = tunnelPool[tunnelPoolIndex - 1].GetComponentInChildren<BoxCollider> ();
+			Vector3 gPos = tunnelPool[tunnelPoolIndex - 1].transform.position;
+            tunnelPool[tunnelPoolIndex - 1].transform.position = new Vector3 (gPos.x+floor.size.y/2, gPos.y, gPos.z);
             startPos = spawnTransform.position;
 			int numberOfLasersSpawned = Random.Range (0, 5);
 			for (int i = 0; i < numberOfLasersSpawned; i++) {
@@ -70,11 +170,35 @@ public class lvlTest : MonoBehaviour {
 				l.transform.position = new Vector3 (gPos.x+Random.Range(0.3f, floor.size.y-0.3f), gPos.y+l.transform.localScale.y, 0.125f);
 			}
         }
-		destroyLaggedObjects ();
+		//destroyLaggedObjects ();
 		spawnShips ();
-		spawnAsteroid ();
+		//spawnAsteroid ();
 	}
+    
+    int RepositionObject(GameObject[] obPool, int obIndex, int obPoolSize, Vector3 pos, Quaternion rot)
+    {
+        obPool[obIndex].transform.position = pos;
+        obPool[obIndex].transform.rotation = rot;
+        if(obIndex >= obPoolSize - 1)
+        {
+            obIndex = 0;
+        }
+        else
+        {
+            obIndex += 1;
+        }
+        Debug.Log(obPool[obIndex].name + ": " + obIndex.ToString());
+        return obIndex;
+    }
 
+    GameObject[] GetGameObjectSlice(GameObject[,] ob2D, GameObject[] ob1D, int index, int poolSize)
+    {
+        for(int i = 0; i < poolSize; i++)
+        {
+            ob1D[i] = ob2D[index, i];
+        }
+        return ob1D;
+    }
 	/// <summary>
 	/// Spawns the Ships. (Or at least the warnings for them...)
 	/// </summary>
@@ -82,9 +206,15 @@ public class lvlTest : MonoBehaviour {
 		shipSpawn -= Time.deltaTime;
 		if (shipSpawn <= 0 && shipSpawnMin > 0) {
 			shipSpawn = Random.Range (shipSpawnMin, shipSpawnMax);
-			GameObject s = Instantiate (ship);
-			Vector3 sPos = s.transform.position;
-			s.transform.position = new Vector3 (sPos.x, spawnTransform.position.y+Random.Range(spawnHeightRange[0]-1.5f,spawnHeightRange[1]+1.5f), sPos.z);
+            shipPoolIndex = RepositionObject(shipPool, shipPoolIndex, shipPoolSize,
+                new Vector3(0, 0, 0),
+                new Quaternion());
+            //shipPoolIndex = RepositionObject(shipPool, shipPoolIndex, shipPoolSize,
+            //    new Vector3(0, spawnTransform.position.y + Random.Range(spawnHeightRange[0] - 1.5f, spawnHeightRange[1] + 1.5f), 0),
+            //    new Quaternion());
+			//GameObject s = Instantiate (ship);
+			//Vector3 sPos = s.transform.position;
+			//s.transform.position = new Vector3 (sPos.x, spawnTransform.position.y+Random.Range(spawnHeightRange[0]-1.5f,spawnHeightRange[1]+1.5f), sPos.z);
 
 		}
 	}
